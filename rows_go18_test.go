279582bfdf48cb4372e75a77200ee78defabce46
Marcel <marcel@mahillmann.de>
2019-01-04 02:18:26 +0100

    mv test cases to GO1.8 version
    Signed-off-by: Marcel <marcel@mahillmann.de>

--- rows_go18_test.go
+++ rows_go18_test.go
@@ -90,3 +90,56 @@ func TestQueryMultiRows(t *testing.T) {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
+
+func TestNewColumnWithDefinition(t *testing.T) {
+	column1 := NewColumn("test", "VARCHAR", "", true, 100, 0, 0)
+	column2 := NewColumn("number", "DECIMAL", float64(0.0), false, 0, 10, 4)
+	rows := NewRowsWithColumnDefiniton(column1, column2).AddRow("foo.bar", float64(10.123))
+
+	db, mock, _ := New()
+	mock.ExpectQuery("SELECT test, number from dummy").WillReturnRows(rows)
+
+	query, _ := db.Query("SELECT test, number from dummy")
+
+	if query.Next() {
+		var test string
+		var number float64
+
+		if queryError := query.Scan(&test, &number); queryError != nil {
+			t.Fatal(queryError)
+		} else if test != "foo.bar" {
+			t.Fatal("field test is not 'foo.bar'")
+		} else if number != float64(10.123) {
+			t.Fatal("field number is not '10.123'")
+		}
+
+		if columnTypes, colTypErr := query.ColumnTypes(); colTypErr != nil {
+			t.Fatal(colTypErr)
+		} else if len(columnTypes) != 2 {
+			t.Fatal("number of columnTypes")
+		} else if name := columnTypes[0].Name(); name != "test" {
+			t.Fatalf("field 'test' has a wrong name '%s'", name)
+		} else if dbTyp := columnTypes[0].DatabaseTypeName(); dbTyp != "VARCHAR" {
+			t.Fatalf("field 'test' has a wrong db type '%s'", dbTyp)
+		} else if columnTypes[0].ScanType().Kind() != reflect.String {
+			t.Fatal("field 'test' has a wrong scanType")
+		} else if precision, scale, _ := columnTypes[0].DecimalSize(); precision != 0 || scale != 0 {
+			t.Fatal("field 'test' has a wrong precision, scale")
+		} else if length, _ := columnTypes[0].Length(); length != 100 {
+			t.Fatalf("field 'test' has a wrong length '%d'", length)
+		} else if name := columnTypes[1].Name(); name != "number" {
+			t.Fatalf("field 'number' has a wrong name '%s'", name)
+		} else if dbTyp := columnTypes[1].DatabaseTypeName(); dbTyp != "DECIMAL" {
+			t.Fatalf("field 'number' has a wrong db type '%s'", dbTyp)
+		} else if columnTypes[1].ScanType().Kind() != reflect.Float64 {
+			t.Fatal("field 'number' has a wrong scanType")
+		} else if precision, scale, _ := columnTypes[1].DecimalSize(); precision != int64(10) || scale != int64(4) {
+			t.Fatal("field 'number' has a wrong precision, scale")
+		} else if length, _ := columnTypes[1].Length(); length != 0 {
+			t.Fatal("field 'number' has a wrong length")
+		}
+	} else {
+		t.Fatal("no result set")
+	}
+
+}
